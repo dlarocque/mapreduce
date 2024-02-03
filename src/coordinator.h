b3d9e1916db72c4b4fec2cd10344830c8c9e91ff
Daniel La Rocque <larocq17@myumanitoba.ca>
2024-02-03 12:31:17 -0600

    Add gRPC and protobuf to CMake project with empty worker and coord

---
+++ src/coordinator.h
@@ -0,0 +1,14 @@
+//
+// Created by Daniel La Rocque on 2024-02-01.
+//
+
+#ifndef MAPREDUCE_COORDINATOR_H
+#define MAPREDUCE_COORDINATOR_H
+
+
+class coordinator {
+
+};
+
+
+#endif //MAPREDUCE_COORDINATOR_H
---
+++ src/worker.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Daniel La Rocque on 2024-02-01.
+//
+
+#include "worker.h"
+
+int main() {
+    // Start the RPC server
+    // Listen for incoming requests
+
+    return 0;
+}
